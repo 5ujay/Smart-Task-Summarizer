a14778c3ae752bec5e7407050492f2795d709fe8
5ujay <sujaypagam777@gmail.com>
2025-07-30 15:19:03 +0530

    modify the frontend service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smart-task-summarizer.onrender.com/",
+  baseURL: "https://smart-task-summarizer.onrender.com/api",
 });
 
 export default api;
